fb2786f0069b548d2dfb93fe72960361188cd391
Jonah Chates <jonah.chates@gmail.com>
2019-06-14 15:15:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,14 +18,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var uppercase = "HELLO"
   var lowercase = "hello"
-  
-  if((string === )); {
-   return  "I can't hear you"
-  }; else if (string === "uppercase"); {
-    return "YES INDEED!"
-  } else (string === "I love you, Grandma"); {
-    return "I love you, too"
-  }
-
-}
    
+   if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  }else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  }else if (string === "I love you, Grandma."){
+    return "I love you, too."}
+}
