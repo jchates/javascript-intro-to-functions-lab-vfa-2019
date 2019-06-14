0009993850ab0bc6cbe7341c4930d0af467b94e4
Jonah Chates <jonah.chates@gmail.com>
2019-06-14 14:56:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,13 +20,6 @@ function sayHiToGrandma(string) {
   if((string) === "lowercase"); {
    return  "I can't hear you"}
 
-  else if((string) === "uppercase"); {
-    return "YES INDEED!"}
-}
-   
- 
- 
- 
-   
   
 }
+   
